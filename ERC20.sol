b81f6b0645b768f6e09278e82651f12b658df75b
Abel Gomez <67193379+xsending@users.noreply.github.com>
2023-11-23 14:52:58 +0800

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -5,8 +5,8 @@ contract ERC20 {
     uint public totalSupply;
     mapping(address => uint) public balanceOf;
     mapping(address => mapping(address => uint)) public allowance;
-    string public name = "Solidity by Example";
-    string public symbol = "SOLBYEX";
+    string public name = "Custom Token";
+    string public symbol = "CTK";
     uint8 public decimals = 18;
 
 		event Transfer(address indexed from, address indexed to, uint value);
